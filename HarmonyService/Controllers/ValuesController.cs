4fa1602b456f8adfbf6fce3b8417c97e2592c3a0
V2NileshSukalikar <nilesh.sukalikar@v2solutions.com>
2017-02-27 18:31:09 +0530

    check view controller

--- HarmonyService/Controllers/ValuesController.cs
+++ HarmonyService/Controllers/ValuesController.cs
@@ -14,6 +14,9 @@ namespace HarmonyService.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+
+
+
         }
 
         // GET api/values/5
