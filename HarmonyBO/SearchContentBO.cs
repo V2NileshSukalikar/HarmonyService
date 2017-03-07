426ae7d2e5273e1284372c82a48a57249d8a9fb7
V2NileshSukalikar <nilesh.sukalikar@v2solutions.com>
2017-03-07 14:31:26 +0530

    API creation
    dummy json data API

---
+++ HarmonyBO/SearchContentBO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarmonyBO
+{
+    public class SearchContentBO
+    {
+        public string ProviderId { get; set; }
+        public string Provider_UUID { get; set; }
+        public string ProviderName { get; set; }
+        public bool IsMailingAddress { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string County { get; set; }
+        public string Municipality { get; set; }
+        public string Phone { get; set; }
+        public string WebSite { get; set; }
+        public string SortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string[] Service { get; set; }
+        public string Neighborhood { get; set; }
+        public string[] Keywords { get; set; }
+        public string ProviderEmail { get; set; }
+        public string ServiceAreaTown { get; set; }
+        public string ServiceAreaZipCode { get; set; }
+        public string ServiceAreaCounty { get; set; }
+        public string ServiceAreaMunicipality { get; set; }
+        public string AddressAndAreaServed { get; set; }
+        public string Notes { get; set; }
+        public string AreaServedServices { get; set; }
+    }
+}
