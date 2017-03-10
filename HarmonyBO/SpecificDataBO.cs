4de6a94d813f52e99ef5e7af808382d511123532
V2NileshSukalikar <nilesh.sukalikar@v2solutions.com>
2017-03-10 16:28:14 +0530

    Json Modification

--- HarmonyBO/SpecificDataBO.cs
+++ HarmonyBO/SpecificDataBO.cs
@@ -7,8 +7,14 @@ namespace HarmonyBO
 {
     public class SpecificDataBO
     {
-        public string Orientation { get; set; }
+        public List<OrientationBO> Orientation { get; set; }
         public bool isCacheble { get; set; }
-        public List<PageSpecificDataBO>  Data { get; set; }
+        public List<PageSpecificDataBO> Data { get; set; }
+    }
+
+    public class OrientationBO
+    {
+        public string Type { get; set; }
+        public string[] Order { get; set; }
     }
 }
